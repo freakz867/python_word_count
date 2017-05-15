8072035b545a362d7e64be54a43e98b9c72ea3fe
Dmitry <uinos@yandex.ru>
2017-05-15 15:47:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ if __name__ == '__main__':
     workTime =  endTime - startTime
          
     #print results
-    print "The job took " + str(workTime) + " seconds to complete"
-    print "The final sum was: " + str(finalSum)
+    print('The job took %s seconds to complete' % str(workTime)
+    print(The final sum was: %s' % str(finalSum))

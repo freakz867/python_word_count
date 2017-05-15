e00172670f21360cffabda9ea715a5c17c7b94ba
Dmitry <uinos@yandex.ru>
2017-05-15 15:20:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
 from math import factorial
-from multiprocessing import Pool
+#from multiprocessing import Pool
 
-def print_factorial (value):
-    return factorial(value)
+def print_factorial ():
+    print('start calculating')
+    return factorial(70000)
 
-if __name__ == '__main__':
-    with Pool(5) as p:
-        print(p.map(print_factorial, [50000, 60000, 70000]))
+#if __name__ == '__main__':
+#    with Pool(5) as p:
+#        print(p.map(print_factorial, [50000, 60000, 70000]))
